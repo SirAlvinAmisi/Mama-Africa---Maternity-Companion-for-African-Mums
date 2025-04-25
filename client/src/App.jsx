6b4cd70f78560beef2fd23b2cb1165d5a7d84c78
Sayfudin90 <Seifdin62@gmail.com>
2025-04-25 20:29:16 +0300

    edited home and about pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
 
   return (
     <div>
       <h1>Mama Afrika</h1>
+      <Home />
+      <About />
       <div>
       </div>
     </div>
