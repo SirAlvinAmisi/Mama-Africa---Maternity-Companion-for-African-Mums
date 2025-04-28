cf8379b0a2941c2e0bd4ed618e3e3d693999ce6e
Alvin Amisi <alvomakaya@gmail.com>
2025-04-28 11:33:52 +0300

    updated App.jsx
    updates routes to the mum pages and solved and error i was experiencing with the same

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
-import About from './pages/About'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import About from './pages/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Login from './components/Login';
 import Signup from './components/SignUp';
 import Communities from './pages/Communities';
@@ -14,15 +15,16 @@ import { Privacy } from './pages/Privacy';
 import { Terms } from './pages/Terms';
 import CommunityGuidelines from './pages/CommunityGuidelines';
 import HelpCenter from './pages/HelpCenter';
-import MomLayout         from './components/MomLayout';
-import MomRegister       from './pages/MomRegister';
-import MomProfile        from './pages/MomProfile';
-import MomPregnancy      from './pages/MomPregnancy';
-import MomDevelopment    from './pages/MomDevelopment';
-import MomReminders      from './pages/MomReminders';
-import MomUploadScan     from './pages/MomUploadScan';
-import MomAskQuestion    from './pages/MomAskQuestion';
-import MomContent        from './pages/MomContent';
+
+import MomLayout      from './components/MomLayout';
+import MomRegister    from './pages/MomRegister';
+import MomProfile     from './pages/MomProfile';
+import MomPregnancy   from './pages/MomPregnancy';
+import MomDevelopment from './pages/MomDevelopment';
+import MomReminders   from './pages/MomReminders';
+import MomUploadScan  from './pages/MomUploadScan';
+import MomAskQuestion from './pages/MomAskQuestion';
+import MomContent     from './pages/MomContent';
 
 function NotFound() {
   return (
@@ -32,32 +34,49 @@ function NotFound() {
     </div>
   );
 }
+
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/specialists" element={<Specialists />} />
-              {/* <Route path="/health-professional" element={<HealthProfessional />} /> */}
-              <Route path="/communities" element={<Communities />}/>
-              <Route path="/health-professional-mom" element={<HealthProfessionalMom />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/privacy" element={<Privacy />} />
-              <Route path="/terms" element={<Terms />} />
-              <Route path="/community-guidelines" element={<CommunityGuidelines />} />
-              <Route path="/help-center" element={<HelpCenter />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
+        <main className="flex-grow">
+          <Routes>
+            {/* Existing public routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/specialists" element={<Specialists />} />
+            {/* <Route path="/health-professional" element={<HealthProfessional />} /> */}
+            <Route path="/communities" element={<Communities />} />
+            <Route path="/health-professional-mom" element={<HealthProfessionalMom />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/community-guidelines" element={<CommunityGuidelines />} />
+            <Route path="/help-center" element={<HelpCenter />} />
+
+            {/* 👶 Mum section */}
+            <Route path="/moms" element={<MomLayout />}>
+              <Route index element={<MomRegister />} />
+              <Route path="register"     element={<MomRegister />} />
+              <Route path="profile"      element={<MomProfile />} />
+              <Route path="pregnancy"    element={<MomPregnancy />} />
+              <Route path="development"  element={<MomDevelopment />} />
+              <Route path="reminders"    element={<MomReminders />} />
+              <Route path="upload-scan"  element={<MomUploadScan />} />
+              <Route path="ask-question" element={<MomAskQuestion />} />
+              <Route path="content"      element={<MomContent />} />
+            </Route>
+
+            {/* Fallback 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
   );
 }
-export default App;
 
+export default App;
