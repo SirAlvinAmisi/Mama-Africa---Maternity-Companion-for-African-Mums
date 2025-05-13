5a6921cdb216e4fcdc5569d310dfb273cc41de45
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-05-13 14:58:23 +0300

    adjusted ui

--- client/src/components/admin/PostReview.jsx
+++ client/src/components/admin/PostReview.jsx
@@ -136,7 +136,7 @@ const PostReview = () => {
           <div className="flex gap-4 mt-2">
             <button
               onClick={() => updateContentStatus(data.id, "approved", flagId, content_type)}
-              className="bg-green-600 text-white px-4 py-2 rounded"
+              className="bg-cyan-500 text-black px-4 py-2 rounded"
             >
               Approve
             </button>
